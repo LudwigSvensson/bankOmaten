c17789ed7d6b75017e715f095d6727ae91fcd704
Ludwig Svensson <ludwig.gifrod@hotmail.com>
2023-10-09 14:04:48 +0200

    Fixade till felhantering i de olika metoderna.

--- Program.cs
+++ Program.cs
@@ -7,14 +7,15 @@ namespace bankOmaten
         static void Main(string[] args)
         {
             int tries = 5;
-            bool go = true;
-            Welcome();
+            
+            
             Thread.Sleep(1000);
             while (tries != 0)
-            {                
+            {
+                Welcome();
                 Showtries(tries);
                 LoggaIn(ref tries);                               
-          }
+            }
             Console.Clear();
             Console.WriteLine("Du har förbrukat dina fem försök för att logga in, now fuck off");
         }
@@ -25,6 +26,7 @@ namespace bankOmaten
             int randyString = randy.Next(welcome.Length);
             string randyWelcome = welcome[randyString];
             Console.WriteLine(randyWelcome);
+            Console.WriteLine("Skriv in användarnamn och lösenord för att logga in.");
         }
 
         static int LoggaIn(ref int tries)
@@ -38,18 +40,17 @@ namespace bankOmaten
                 var pw = Convert.ToInt32(Console.ReadLine());
                 if (pw == 123456 && (users.Contains(user)))
                 {
+                    Console.Clear();
+                    Console.WriteLine("Inloggning lyckades!");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("..........\n........... \n............");
+                    Thread.Sleep(1500);
+                    Console.Clear();
                     tries = 6;
                     while (tries ==6)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Inloggning lyckades!");
-                        Thread.Sleep(1000);
-                        Console.WriteLine("..........\n........... \n............");
-                        Thread.Sleep(1500);
-                        Console.Clear();
-                        Bankmeny(ref tries, user);
-                    }  
-                    
+                    {                                               
+                        Bankmeny(ref tries, user);                                            
+                    }                      
                     return tries;  
                 }
 
@@ -74,33 +75,67 @@ namespace bankOmaten
         }
         static int Bankmeny(ref int tries,string user)
         {
+           
             Console.WriteLine($"Välkommen {user}");
-            Console.WriteLine("[1]. Se dina konton och dess saldon\n[2]. Se dina konton och dess saldon\n[3]. Se dina konton och dess saldon\n[4]. Logga ut");
-            int uInput = Convert.ToInt32(Console.ReadLine());
-
-            switch (uInput)
+            Console.WriteLine("[1]. Se dina konton och dess saldon\n[2]. Överföring mellan konton\n[3]. Ta ut pengar\n[4]. Logga ut");
+            try
             {
-                case 1:
-                    Console.WriteLine("[1]. Se dina konton och dess saldon");
-                    break;
-                case 2:
-                    Console.WriteLine("[2]. Se dina konton och dess saldon");
-                    break;
-                case 3:
-                    Console.WriteLine("[3]. Se dina konton och dess saldon");
-                    break;
-                case 4:
-                    Console.WriteLine("[4]. Logga ut");                    
-                    Console.Clear();
-                    Console.WriteLine("Återvänder till Inloggning.......");
-                    Thread.Sleep(1500);
-                    Console.Clear();
-                    return tries--;
-                    break;
-                default:
-                    Console.WriteLine("Ogiltigt val, försök igen");
-                    break;
+                int uInput = Convert.ToInt32(Console.ReadLine());
+            
+                switch (uInput)
+                {
+                    case 1:
+                        Console.WriteLine("[1]. Se dina konton och dess saldon");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("[2]. Överföring mellan konton");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("[3]. Ta ut pengar");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("[4]. Logga ut");
+                        Console.Clear();
+                        Console.WriteLine("Återvänder till Inloggning.......");
+                        Thread.Sleep(1500);
+                        Console.Clear();                        
+                        return tries--;
+                        break;
+                        
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Ogiltigt val, försök igen");
+                        Thread.Sleep(1500);
+                        Console.Clear();
+                        break;
+                }
             }
+            catch (FormatException)
+            {
+                Console.Clear();
+                Console.WriteLine("Du måste ange en av siffrorna: 1, 2, 3 eller 4.");
+                Thread.Sleep(1500);
+                Console.Clear();
+            }
+            catch (OverflowException)
+            {
+                Console.Clear();
+                Console.WriteLine("Du måste ange en av siffrorna: 1, 2, 3 eller 4.");
+                Thread.Sleep(1500);
+                Console.Clear();
+            }  
+            catch(Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Vänligen välj ett utav alternativen. 1, 2, 3 eller 4.");
+                Thread.Sleep(1500);
+                Console.Clear();
+            }
+
             return tries;
         }
     }
