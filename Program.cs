49fca645b0b00034a72e9ba42be7f05c1aea95cd
Ludwig Svensson <ludwig.gifrod@hotmail.com>
2023-10-12 15:31:30 +0200

    Programmet är "färdigt". Behöver finputsa lite, lägga till felhantering osv.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,37 @@
-﻿using System.Reflection.Metadata;
+﻿using Microsoft.Win32;
+using System.Data;
+using System.Reflection.Metadata;
 
 namespace bankOmaten
 {
     internal class Program
     {
+        private static decimal ludwigsKortKonto = 100.50m;
+        private static decimal ludwigSparKonto = 1000.50m;
+        private static decimal ludwigsMEGAsparKonto = 10000.50m;
+        private static decimal anasKortKonto = 200.50m;
+        private static decimal anasSparKonto = 2000.50m;
+        private static decimal marcusKortKonto = 300.50m;
+        //private static Dictionary<string, decimal> ludwigsKonton = new Dictionary<string, decimal>();
+        //private static Dictionary<string, decimal> anasKonton = new Dictionary<string, decimal>();
+
+
+
         static void Main(string[] args)
         {
-            int tries = 5;
-            
-            
+            //ludwigsKonton["Kortkonto"] = 100.50m;
+            //ludwigsKonton["Sparkonto"] = 1000.50m;
+            //ludwigsKonton["MEGAsparkonto"] = 10000.50m;
+            //decimal kortKonto = ludwigsKonton["Kortkonto"];
+            //Console.WriteLine(kortKonto);
+            //anasKonton["Kortkonto"] = 200.50m;
+            //anasKonton["Sparkonto"] = 2000.50m;
+            //string[] acc = new string[3];
+            //acc[0] = "[Kortkonto]";
+            //acc[1] = "[SparKonto]";
+            //acc[2] = "[MEGASparkonto]";
+
+            int tries = 5;            
             Thread.Sleep(1000);
             while (tries != 0)
             {
@@ -27,7 +50,7 @@ namespace bankOmaten
             string randyWelcome = welcome[randyString];
             Console.WriteLine(randyWelcome);
             Console.WriteLine("Skriv in användarnamn och lösenord för att logga in.");
-        }
+        }//Metod som skriver ut välkomstmeddelande
 
         static int LoggaIn(ref int tries)
         {
@@ -58,6 +81,7 @@ namespace bankOmaten
                 {
                     Console.Clear();
                     Console.WriteLine("FEL! \nVänligen ange rätt användarnamn och lösenord");
+                    Thread.Sleep(2000);                    
                     return tries--;
                 }              
             }
@@ -67,12 +91,12 @@ namespace bankOmaten
                 Console.WriteLine("Du börjar med att skriva in ditt användarnamn, \n efter det din SEXsiffriga pinkod");
                 return tries--;
             }
-        }       
+        } //Metod som används för att logga in i 'banken' 
         static void Showtries(int tries)
         {                     
                 Console.WriteLine($"Du har {tries} försök kvar");
                 Thread.Sleep(1000);  
-        }
+        }//Metod som visar skriver ut hur många försök man har kvar på att logga in
         static int Bankmeny(ref int tries,string user)
         {
            
@@ -101,23 +125,35 @@ namespace bankOmaten
                         break;
 
                     case 2:
-                        Console.WriteLine("[2]. Överföring mellan konton");
+                        Console.Clear();
+                        if (user == "ludwig")
+                        {
+                            LudwigsTransfer();                   
+                        }
+                        else if (user == "anas")
+                        {
+                            AnasTransfer();
+                        }
+                        else if (user == "marcus")
+                        {
+                            Console.WriteLine("Du har bara ett konto...");
+                            MarcusKonton();
+                        }
+                        
                         break;
 
                     case 3:
-                        Console.WriteLine("[3]. Ta ut pengar");
+                        Console.Clear();
+                        withdrawStåler(user);
                         break;
-
-                    case 4:
-                        Console.WriteLine("[4]. Logga ut");
+                                              
+                    case 4:                        
                         Console.Clear();
                         Console.WriteLine("Återvänder till Inloggning.......");
                         Thread.Sleep(1500);
                         Console.Clear();                        
                         return tries--;
-                        break;
                         
-
                     default:
                         Console.Clear();
                         Console.WriteLine("Ogiltigt val, försök igen");
@@ -149,35 +185,306 @@ namespace bankOmaten
             }
 
             return tries;
-        }
+        }//Metod för 'menyn' som visas när man loggat in i 'banken'
         
-       static void LudwigsKonton()
-       {
-            Console.Clear(); 
-            double kortKonto = 100.50;
-            double sparkonto = 1000.50;
-            Console.WriteLine($"{kortKonto}:-");
-            Console.ReadKey();
+        static void LudwigsKonton()
+       {                      
+            Console.WriteLine($" [Kortkonto] {ludwigsKortKonto};- \n [Sparkonto] {ludwigSparKonto};- \n [MEGASparkonto] {ludwigsMEGAsparKonto};- ");
+            Console.WriteLine("Vänligen trycker Enter för att gå tillbaka");
+            Console.ReadLine();
             Console.Clear();
-        }
+        }//Visar saldo för Ludwigs konton
         static void AnasKonton()
         {
             Console.Clear();
-            double kortKonto = 100.49;
-            double sparkonto = 1000.49;
-            Console.WriteLine($"{kortKonto}");
-            Console.ReadKey();
+            Console.WriteLine($"[Kortkonto] {anasKortKonto};- \n [Sparkonto] {anasSparKonto};-");
+            Console.WriteLine("Vänligen trycker Enter för att gå tillbaka");
+            Console.ReadLine();
             Console.Clear();
-        }
+        }//Visar saldo för Anas konton
         static void MarcusKonton()
         {
+            Console.Clear();            
+            Console.WriteLine($"[Kortkonto] {marcusKortKonto};-");
+            Console.WriteLine("Vänligen trycker Enter för att gå tillbaka");
+            Console.ReadLine();
             Console.Clear();
-            double kortKonto = 100.48;
-            double sparKonto = 1000.48;
-            Console.WriteLine($"{kortKonto}");
-            Console.ReadKey();
-            Console.Clear();
-        }
+        }//Visar saldo för Marcus konton
+        static void LudwigsTransfer()
+        {
+            try
+            {
+                Console.WriteLine($"Välj konto du vill föra över ifrån \n [1][Kortkonto]{ludwigsKortKonto} \n [2][Sparkonto]{ludwigSparKonto} \n [3][MEGASparkonto]{ludwigsMEGAsparKonto}");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Console.WriteLine($"Välj konto du vill föra över till \n [1][Sparkonto]{ludwigSparKonto} \n [2][MEGASparkonto]{ludwigsMEGAsparKonto}");
+                    int choice2 = Convert.ToInt32(Console.ReadLine());
+                    if (choice2 == 1)
+                    {
+                        transferStålar(ref ludwigsKortKonto, ref ludwigSparKonto);
+                    }
+                    else if (choice2 == 2)
+                    {
+                        transferStålar(ref ludwigsKortKonto, ref ludwigsMEGAsparKonto);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vänligen ange [1] eller [2]");
+                    }
 
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine($"Välj konto du vill föra över till \n [1][Kortkonto]{ludwigsKortKonto} \n [2][MEGASparkonto]{ludwigsMEGAsparKonto}");
+                    int choice2 = Convert.ToInt32(Console.ReadLine());
+                    if (choice2 == 1)
+                    {
+                        transferStålar(ref ludwigSparKonto, ref ludwigsKortKonto);
+                    }
+                    else if (choice2 == 2)
+                    {
+                        transferStålar(ref ludwigSparKonto, ref ludwigsMEGAsparKonto);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vänligen ange [1] eller [2]");
+                    }
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine($"Välj konto du vill föra över till \n [1][Kortkonto]{ludwigsKortKonto} \n [2][Sparkonto]{ludwigSparKonto}");
+                    int choiceL2 = Convert.ToInt32(Console.ReadLine());
+                    if (choiceL2 == 1)
+                    {
+                        transferStålar(ref ludwigsMEGAsparKonto, ref ludwigsKortKonto);
+                    }
+                    else if (choiceL2 == 2)
+                    {
+                        transferStålar(ref ludwigsMEGAsparKonto, ref ludwigSparKonto);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vänligen ange [1] eller [2]");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltigt val, försök igen");
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ogiltigt val, försök igen");
+            }
+           
+        }//Metod för att föra över pengar emellan användare Ludwigs konton
+        static void AnasTransfer()
+        {
+            try
+            {
+                Console.WriteLine($"Välj konto du vill föra över ifrån \n [1][Kortkonto]{anasKortKonto} eller [2][Sparkonto]{anasSparKonto}");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Console.WriteLine($"Välj konto du vill föra över till \n [1][Kortkonto]{anasSparKonto}");
+                    int choice2 = Convert.ToInt32(Console.ReadLine());
+                    if (choice2 == 1)
+                    {
+                        transferStålar(ref anasKortKonto, ref anasSparKonto);
+                    }
+                    else if (choice2 == 2)
+                    {
+                        transferStålar(ref anasSparKonto, ref anasKortKonto);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vänligen ange [1] eller [2]");
+                    }
+
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine($"Välj konto du vill föra över till \n [1][Kortkonto]{anasKortKonto}");
+                    int choice2 = Convert.ToInt32(Console.ReadLine());
+                    if (choice2 == 1)
+                    {
+                        transferStålar(ref anasSparKonto, ref anasKortKonto);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vänligen ange ett giltigt val \n Vänligen välj [1]");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltigt val, försök igen");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ogiltigt val, försök igen");              
+            }
+
+        }//Metod för att föra över pengar emellan användare Anas konton
+        static void withdrawStåler(string user)
+        {
+            if (user == "ludwig")
+            {
+                Console.Write("Vänligen ange din sexsiffriga pinkod för att fortsätta: ");
+                var pw = Convert.ToInt32(Console.ReadLine());
+                if (pw == 123456)
+                {
+                    Console.WriteLine($"Välj konto att ta ut pengar ifrån: \n [1][Kortkonto]{ludwigsKortKonto} \n [2][Sparkonto]{ludwigSparKonto} \n [3][MEGAsparkonto]{ludwigsMEGAsparKonto}");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"[1][Kortkonto]{ludwigsKortKonto}");
+                        Console.WriteLine("Hur mycket vill du ta ut? ");
+                        decimal remove = Convert.ToDecimal(Console.ReadLine());
+                        if (remove <= ludwigsKortKonto)
+                        {
+                            ludwigsKortKonto -= remove;
+                            Console.WriteLine($"Uttaget är genomfört. Kvar på kontot: {ludwigsKortKonto}");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Du kan inte ta ut mer pengar än vad du har på ditt konto");
+                        }
+                    }
+                    else if (choice == 2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"[2][Sparkonto]{ludwigSparKonto}");
+                        Console.WriteLine("Hur mycket vill du ta ut? ");
+                        decimal remove = Convert.ToDecimal(Console.ReadLine());
+                        if (remove <= ludwigSparKonto)
+                        {
+                            ludwigSparKonto -= remove;
+                            Console.WriteLine($"Uttaget är genomfört. Kvar på kontot: {ludwigSparKonto}");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Du kan inte ta ut mer pengar än vad du har på ditt konto");
+                        }
+                    }
+                    else if (choice == 3)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"[3][MEGASparkonto]{ludwigsMEGAsparKonto}");
+                        Console.WriteLine("Hur mycket vill du ta ut? ");
+                        decimal remove = Convert.ToDecimal(Console.ReadLine());
+                        if (remove <= ludwigsMEGAsparKonto)
+                        {
+                            ludwigsMEGAsparKonto -= remove;
+                            Console.WriteLine($"Uttaget är genomfört. Kvar på kontot: {ludwigsMEGAsparKonto}");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Du kan inte ta ut mer pengar än vad du har på ditt konto");
+                        }
+                    }
+                }
+            }
+            else if (user == "anas")
+            {
+                Console.Write("Vänligen ange din sexsiffriga pinkod för att fortsätta: ");
+                var pw = Convert.ToInt32(Console.ReadLine());
+                if (pw == 123456)
+                {
+                    Console.WriteLine($"Välj konto att ta ut pengar ifrån: \n [1][Kortkonto]{anasKortKonto} \n [2][Sparkonto]{anasSparKonto}");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"[1][Kortkonto]{anasKortKonto}");
+                        Console.WriteLine("Hur mycket vill du ta ut? ");
+                        decimal remove = Convert.ToDecimal(Console.ReadLine());
+                        if (remove <= anasKortKonto)
+                        {
+                            anasKortKonto -= remove;
+                            Console.WriteLine($"Uttaget är genomfört. Kvar på kontot: {anasKortKonto}");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Du kan inte ta ut mer pengar än vad du har på ditt konto");
+                        }
+
+                    }
+                    else if (choice == 2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"[2][Sparkonto]{anasSparKonto}");
+                        Console.WriteLine("Hur mycket vill du ta ut? ");
+                        decimal remove = Convert.ToDecimal(Console.ReadLine());
+                        if (remove <= anasSparKonto)
+                        {
+                            anasSparKonto -= remove;
+                            Console.WriteLine($"Uttaget är genomfört. Kvar på kontot: {anasSparKonto}");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Du kan inte ta ut mer pengar än vad du har på ditt konto");
+                        }
+                    }
+                }
+            }
+            else if (user == "marcus")
+            {
+                Console.Write("Vänligen ange din sexsiffriga pinkod för att fortsätta: ");
+                var pw = Convert.ToInt32(Console.ReadLine());
+                if (pw == 123456)
+                {
+                    Console.WriteLine($"Välj konto att ta ut pengar ifrån: \n [1][Kortkonto]{marcusKortKonto}");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"[1][Kortkonto]{marcusKortKonto}");
+                        Console.WriteLine("Hur mycket vill du ta ut? ");
+                        decimal remove = Convert.ToDecimal(Console.ReadLine());
+                        if (remove <= marcusKortKonto)
+                        {
+                            marcusKortKonto -= remove;
+                            Console.WriteLine($"Uttaget är genomfört. Kvar på kontot: {marcusKortKonto}");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Du kan inte ta ut mer pengar än vad du har på ditt konto");
+                        }
+
+                    }
+                }
+            }
+        }//Metod för att ta ut pengar ifrån användarnas konton
+
+        static void transferStålar(ref decimal frånKonto, ref decimal tillKonto)
+        {
+            
+
+            Console.WriteLine($"Hur mycket pengar vill du överföra från {frånKonto} till {tillKonto}?");
+            decimal överföring = Convert.ToDecimal(Console.ReadLine());
+            
+            if (överföring > 0 && överföring <= frånKonto)
+            {
+                frånKonto -= överföring;
+                tillKonto += överföring;
+                Console.WriteLine("Överföringen lyckades! \n Nya saldon:");
+                LudwigsKonton();
+                Thread.Sleep(1500);
+            }
+            else
+            {
+                Console.WriteLine("Ogiltigt belopp eller otillräckligt saldo.");
+            }
+        } //Metod som genomför överföring av pengar emellan konton         
     }
 }
